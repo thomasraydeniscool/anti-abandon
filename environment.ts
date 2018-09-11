4111e6d050d4196d4129559e4142e502e4db5593
Thomas Rayden <thomasmwrayden@gmail.com>
2018-09-11 19:07:29 +0800

    setup shopify-express

--- environment.ts
+++ environment.ts
@@ -1,6 +1,8 @@
-import { cleanEnv, str } from 'envalid';
+import { cleanEnv, str, num } from 'envalid';
 
 export const env = cleanEnv(process.env, {
+  PORT: num({ default: 3000 }),
+  ORIGIN: str({ default: '' }),
   SHOPIFY_KEY: str(),
   SPOPIFY_SECRET: str()
 });
--- index.ts
+++ index.ts
@@ -2,5 +2,36 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import * as express from 'express';
+import * as session from 'express-session';
+
+import * as ShopifyExpress from 'shopify-express';
+import * as ShopifyClient from 'shopify-api-node';
 
 import { env } from './environment';
+
+const app = express();
+
+app.use(session({ secret: env.SPOPIFY_SECRET }));
+
+const shopify = ShopifyExpress({
+  host: env.ORIGIN,
+  apiKey: env.SHOPIFY_KEY,
+  secret: env.SPOPIFY_SECRET,
+  scope: ['write_orders, write_products'],
+  afterAuth(req, res) {
+    const {
+      session: { shop, accessToken }
+    } = req;
+    /**
+     * Install webhooks or hook into your own app here
+     */
+    return res.redirect('/');
+  }
+});
+
+app.use('/shopify', shopify.routes);
+
+app.listen(env.PORT, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Server started on port ${env.PORT}`);
+});
