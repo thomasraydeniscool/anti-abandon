4775a86f34a5ad86c72025290d81183c9f011009
Thomas Rayden <thomasmwrayden@gmail.com>
2018-09-12 00:48:42 +0800

    added a cross to the popup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import styled from "styled-components";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
-import { SubscribeForm } from "./subscribe";
-import { Social } from "./social";
+import { SubscribeForm } from './subscribe';
 
 const Wrapper = styled.div`
   display: block;
@@ -28,19 +27,19 @@ const Container = styled.div`
 `;
 
 const PopupBackground = styled.div`
-    visibility: visible;
-    background-color: #000;
-    -ms-filter: "alpha(opacity=70)";
-    filter: alpha(opacity=70);
-    -moz-opacity: .7;
-    -khtml-opacity: .7;
-    opacity: .7;
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1980000;
+  visibility: visible;
+  background-color: #000;
+  -ms-filter: 'alpha(opacity=70)';
+  filter: alpha(opacity=70);
+  -moz-opacity: 0.7;
+  -khtml-opacity: 0.7;
+  opacity: 0.7;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1980000;
 `;
 
 const Title = styled.h2`
@@ -63,22 +62,16 @@ const Close = styled.button`
   position: absolute;
   top: 20px;
   right: 20px;
-  width: 13px;
-  height: 13px;
+  width: 15px;
+  height: 15px;
   border: 0;
   padding: 0;
   -webkit-box-shadow: none;
   -moz-box-shadow: none;
   box-shadow: none;
-`;
-
-const Line = styled.div`
-  border-top: 1px solid;
-  border-color: #999999;
-  height: 0;
-  width: 100%;
-  margin: 5px 0;
-  display: inline-block;
+  background: url(assets/x.svg);
+  background-size: 15px;
+  text-indent: -9999px;
 `;
 
 const Section = styled.div`
@@ -105,10 +98,10 @@ function App() {
   );
 }
 
-const root = document.createElement("div");
+const root = document.createElement('div');
 
-root.setAttribute("id", "root");
+root.setAttribute('id', 'root');
 
 document.body.appendChild(root);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
