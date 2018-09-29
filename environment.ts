80e3bdae8148f60724d06523e8f9c4191bf5db81
Thomas Rayden <thomasmwrayden@gmail.com>
2018-09-29 11:24:20 +0800

    created mongoose data store strategy

--- environment.ts
+++ environment.ts
@@ -4,7 +4,8 @@ export const env = cleanEnv(process.env, {
   PORT: num({ default: 3000 }),
   ORIGIN: str({ default: '' }),
   SHOPIFY_KEY: str(),
-  SPOPIFY_SECRET: str()
+  SHOPIFY_SECRET: str(),
+  DATABASE: str()
 });
 
 export default env;
--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 import * as express from 'express';
 import * as session from 'express-session';
 
+import * as mongoose from 'mongoose';
+
 import * as ShopifyExpress from 'shopify-express';
 import * as ShopifyClient from 'shopify-api-node';
 
@@ -11,12 +13,48 @@ import { env } from './environment';
 
 const app = express();
 
-app.use(session({ secret: env.SPOPIFY_SECRET }));
+app.use(session({ secret: env.SHOPIFY_SECRET }));
+
+class MongooseStrategy {
+  private Shop: any;
+  private ShopSchema: any;
+
+  constructor(uri: string, options?: any, callback?: () => void) {
+    mongoose.connect(
+      uri,
+      options,
+      callback
+    );
+
+    this.ShopSchema = new mongoose.Schema({
+      shopify_domain: {
+        type: String,
+        unique: true
+      },
+      access_token: {
+        type: String
+      }
+    });
+
+    this.Shop = mongoose.model('Shop', this.ShopSchema);
+  }
+
+  public async storeShop({ shop, accessToken }) {
+    await this.Shop.create({ shopify_domain: shop, access_token: accessToken });
+
+    return { accessToken };
+  }
+
+  public async getShop({ shop }) {
+    const result = await this.Shop.findOne({ shopify_domain: shop });
+    return result || {};
+  }
+}
 
 const shopify = ShopifyExpress({
   host: env.ORIGIN,
   apiKey: env.SHOPIFY_KEY,
-  secret: env.SPOPIFY_SECRET,
+  secret: env.SHOPIFY_SECRET,
   scope: ['write_orders, write_products'],
   afterAuth(req, res) {
     const {
@@ -26,7 +64,8 @@ const shopify = ShopifyExpress({
      * Install webhooks or hook into your own app here
      */
     return res.redirect('/');
-  }
+  },
+  shopStore: new MongooseStrategy(env.DATABASE)
 });
 
 app.use('/shopify', shopify.routes);
