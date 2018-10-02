f262664202897809122d1fb1f82aaee2bd268077
Thomas Rayden <thomasmwrayden@gmail.com>
2018-10-02 19:35:57 +0800

    settings form prototype

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { AppProvider, Page } from '@shopify/polaris';
+import { AppProvider, Layout, Page } from '@shopify/polaris';
+import Settings from './Settings';
+import Preview from './Preview';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,12 +12,14 @@ class App extends React.Component {
     return (
       <AppProvider>
         <Page title="Email Gatherer">
-          <header>
-            <h1>Welcome to React</h1>
-          </header>
-          <p>
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Layout>
+            <Layout.Section>
+              <Preview />
+            </Layout.Section>
+            <Layout.Section>
+              <Settings />
+            </Layout.Section>
+          </Layout>
         </Page>
       </AppProvider>
     );
---
+++ client/Settings.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Card, FormLayout, TextField, Button, Form } from '@shopify/polaris';
+import { Formik } from 'formik';
+
+class Settings extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      header: 'Pick a card',
+      subHeader: '',
+      button: 'Subscribe'
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  public handleChange(value, id) {
+    this.setState(prevState => ({ ...prevState, [id]: value }));
+  }
+
+  public render() {
+    return (
+      <Card title="Settings" sectioned>
+        <Form onSubmit={null}>
+          <FormLayout>
+            <TextField
+              id="header"
+              label="Header text"
+              value={this.state.header}
+              onChange={this.handleChange}
+            />
+            <TextField
+              id="subHeader"
+              label="Sub header text"
+              value={this.state.subHeader}
+              onChange={this.handleChange}
+            />
+            <TextField
+              id="button"
+              label="Button text"
+              value={this.state.button}
+              onChange={this.handleChange}
+            />
+            <Button primary>Save</Button>
+          </FormLayout>
+        </Form>
+      </Card>
+    );
+  }
+}
+
+export default Settings;
