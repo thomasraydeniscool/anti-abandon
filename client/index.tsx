92b52fb183d051b86d649693379aefc602ef14a8
Thomas Rayden <thomasmwrayden@gmail.com>
2018-10-02 18:21:00 +0800

    removed service worker script

--- client/index.tsx
+++ client/index.tsx
@@ -3,10 +3,6 @@ import * as ReactDOM from 'react-dom';
 
 import App from './App';
 
-import registerServiceWorker from './registerServiceWorker';
-
 declare const document: any;
 
 ReactDOM.hydrate(<App />, document.getElementById('root'));
-
-registerServiceWorker();
